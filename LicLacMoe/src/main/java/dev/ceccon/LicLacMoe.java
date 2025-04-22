a75a7fa62996061a2d81d99bfeeab7b330ff6370
Tiago Ceccon <ceccon.t@hotmail.com>
2025-04-21 23:26:37 -0300

    Bootstrap maven project

---
+++ LicLacMoe/src/main/java/dev/ceccon/LicLacMoe.java
@@ -0,0 +1,9 @@
+package dev.ceccon;
+
+public class LicLacMoe {
+
+    public static void main( String[] args ) {
+        System.out.println("Starting LicLacMoe...");
+    }
+
+}
---
+++ LicLacMoe/src/test/java/dev/ceccon/LicLacMoeTest.java
@@ -0,0 +1,14 @@
+package dev.ceccon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LicLacMoeTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
