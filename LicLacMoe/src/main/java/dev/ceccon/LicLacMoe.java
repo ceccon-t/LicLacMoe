6be428a1dff74f2380b5ba1b11087fcac8025642
Tiago Ceccon <ceccon.t@hotmail.com>
2025-04-24 23:51:56 -0300

    Add main view screen

--- LicLacMoe/src/main/java/dev/ceccon/LicLacMoe.java
+++ LicLacMoe/src/main/java/dev/ceccon/LicLacMoe.java
@@ -1,9 +1,17 @@
 package dev.ceccon;
 
+import dev.ceccon.gui.MainView;
+
+import javax.swing.*;
+
 public class LicLacMoe {
 
     public static void main( String[] args ) {
         System.out.println("Starting LicLacMoe...");
+
+        SwingUtilities.invokeLater(() -> {
+            new MainView();
+        });
     }
 
 }
---
+++ LicLacMoe/src/main/java/dev/ceccon/gui/MainView.java
@@ -0,0 +1,17 @@
+package dev.ceccon.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainView extends JFrame {
+
+    public MainView() {
+        super("LicLacMoe");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setSize(400, 400);
+        setLayout(new BorderLayout());
+        setVisible(true);
+    }
+
+}
